ea9608ccd9e66ccd8bfdd92f04abbdd5589019bb
Oleksandr Kulchytskyi <sashaas@inbox.ru>
2012-12-17 23:01:30 +0200

    significant changes have been made

--- ShareDeployed/ShareDeployed.Mailgrabber/Infrastructure/LinkManager.cs
+++ ShareDeployed/ShareDeployed.Mailgrabber/Infrastructure/LinkManager.cs
@@ -6,6 +6,10 @@ using System.Collections.Generic;
 using System.IO;
 using System.Linq;
 using System.Text;
+using System.Reactive;
+using System.Reactive.Linq;
+using System.Reactive.Concurrency;
+using System.Windows;
 
 namespace ShareDeployed.Mailgrabber.Infrastructure
 {
@@ -38,10 +42,10 @@ namespace ShareDeployed.Mailgrabber.Infrastructure
 			if (File.Exists(fname))
 				File.Delete(fname);
 
-			using(StreamWriter sw=new StreamWriter(fname))
+			using (StreamWriter sw = new StreamWriter(fname))
 			{
 				sw.WriteLine(string.Format("EntryId, Key, MsgId"));
-				foreach(var item in _container)
+				foreach (var item in _container)
 				{
 					sw.WriteLine(string.Format("{0},{1},{2}", item.EntryId, item.Key, item.MsgId == null ? string.Empty : item.MsgId));
 				}
@@ -49,6 +53,7 @@ namespace ShareDeployed.Mailgrabber.Infrastructure
 			}
 		}
 
+		[Obsolete("For better performance use LoadFromFileAsync, especially in case of large link files")]
 		internal void LoadFromFile(string fname)
 		{
 			if (!File.Exists(fname))
@@ -58,18 +63,56 @@ namespace ShareDeployed.Mailgrabber.Infrastructure
 			{
 				bool isFirstLine = true;
 				string line;
-				while((line=sr.ReadLine())!=null)
+				while ((line = sr.ReadLine()) != null)
 				{
-					if(isFirstLine)
+					if (isFirstLine)
 					{
 						isFirstLine = false;
 						continue;
 					}
-					string[] data=line.Split(',');
+					string[] data = line.Split(',');
 
 					_container.Add(new OutlookToServerLink() { EntryId = data[0], Key = int.Parse(data[1]), MsgId = data[2] });
 				}
 			}
+		}
+
+		internal void LoadFromFileAsync(string fname)
+		{
+			if (!File.Exists(fname))
+				return;
+			ObserveLines(fname).Skip(1).Subscribe(line =>
+			{
+				string[] data = line.Split(',');
+
+				_container.Add(new OutlookToServerLink() { EntryId = data[0], Key = int.Parse(data[1]), MsgId = data[2] });
+			},
+			ex => MessageBox.Show(ex.Message),
+			() => MessageBox.Show("Loading complete"));
+		}
+
+		IEnumerable<string> ReadLines(Stream stream)
+		{
+			using (StreamReader reader = new StreamReader(stream))
+			{
+				while (!reader.EndOfStream)
+					yield return reader.ReadLine();
+			}
+		}
+
+		IEnumerable<string> ReadLines(string fname)
+		{
+			return ReadLines(File.OpenRead(fname));
+		}
+
+		IObservable<string> ObserveLines(string inputFile)
+		{
+			return ReadLines(inputFile).ToObservable(Scheduler.NewThread);
+		}
+
+		IObservable<string> ObserveLines(Stream inputStream)
+		{
+			return ReadLines(inputStream).ToObservable(Scheduler.NewThread);
 		}
 	}
 }
