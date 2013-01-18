9bbf3def91375b6495840d2b98a4068dc1b5aef3
Oleksandr Kulchytskyi <sashaas@inbox.ru>
2013-01-18 12:48:47 +0200

    Security/Secure

---
+++ ShareDeployed/ShareDeployed.Common/Security/Secure.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+
+namespace ShareDeployed.Common.Security
+{
+	public class Secure
+	{
+		RijndaelManaged Crypto(string keySeed, string saltString)
+		{
+			byte[] salt = UTF8Encoding.UTF8.GetBytes(saltString);
+			using (Rfc2898DeriveBytes derivedBytes = new Rfc2898DeriveBytes(keySeed, salt, 1000))
+			{
+				RijndaelManaged cryprto = new RijndaelManaged();
+				cryprto.KeySize = 128;
+				cryprto.Key = derivedBytes.GetBytes(16);
+				cryprto.IV = derivedBytes.GetBytes(16);
+				return cryprto;
+			}
+		}
+
+	}
+}
