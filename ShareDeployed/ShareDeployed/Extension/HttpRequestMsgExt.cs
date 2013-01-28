a975b623e0a1c08d0113984b7e780afedb6de132
Oleksandr Kulchytskyi <sashaas@inbox.ru>
2013-01-28 12:10:14 +0200

    Added some helper module for HttpRequestMessage. some fix in web.config

---
+++ ShareDeployed/ShareDeployed/Extension/HttpRequestMsgExt.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Web;
+
+namespace ShareDeployed.Extension
+{
+	public static class HttpRequestMsgExt
+	{
+		public static bool IsRequestLocal(this HttpRequestMessage msg)
+		{
+			var locFlag=msg.Properties["MS_IsLocal"] as Lazy<bool>;
+			return (locFlag!=null && locFlag.Value);
+		}
+	}
+}
