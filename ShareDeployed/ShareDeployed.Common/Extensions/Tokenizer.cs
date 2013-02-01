d5997f11c606c35da9a2c39e0dfa00f3c9d35a1e
Oleksandr Kulchytskyi <sashaas@inbox.ru>
2013-02-01 14:10:59 +0200

    Tokenizer module has been implemented.

---
+++ ShareDeployed/ShareDeployed.Common/Extensions/Tokenizer.cs
@@ -0,0 +1,27 @@
+﻿namespace ShareDeployed.Common.Extensions
+{
+	using System.Collections.Generic;
+	using System.Linq;
+	using System.Text.RegularExpressions;
+
+	public static class Tokenizer
+	{
+		public static IEnumerable<KeyValuePair<string, int>> Tokenize(string content)
+		{
+			string[] tokens = { "===", "!==", "==", "<=", ">=", "!=", "-=", "+=", "*=", "/=", "|=", "%=", "^=", ">>=", ">>>=", "<<=",
+                "++", "--", "+", "-", "*", "\\", "/", "&&", "||", "&", "|", "%", "^", "~", "<<", ">>>", ">>",
+                "[", "]", "(", ")", ";", ".", "!", "?", ":", ",", "'", "\"", "{", "}" };
+			var escapedTokens = from token in tokens
+								select ("\\" + string.Join("\\",
+								(from c in token.ToCharArray() select c.ToString()).ToArray()));
+
+			string pattern = "[a-zA-Z_0-9\\$]+|" + string.Join("|", escapedTokens.ToArray());
+			var r = new Regex(pattern, RegexOptions.Compiled | RegexOptions.ExplicitCapture);
+			
+			return from m in r.Matches(content).Cast<Match>()
+				   group m by m.Value into g
+				   orderby g.Count() descending
+				   select new KeyValuePair<string, int>(g.Key, g.Count());
+		}
+	}
+}
