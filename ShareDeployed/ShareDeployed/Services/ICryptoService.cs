dcdb7f5758ef4fd30c73d45b359e5f47a03dd0f0
Oleksandr Kulchytskyi <sashaas@inbox.ru>
2013-04-09 11:13:04 +0300

    ICryptoService was extended, new CryptoHelper class was created.

--- ShareDeployed/ShareDeployed/Services/ICryptoService.cs
+++ ShareDeployed/ShareDeployed/Services/ICryptoService.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using ShareDeployed.Infrastructure;
+using System;
 using System.Security.Cryptography;
 
 namespace ShareDeployed.Services
@@ -6,10 +7,24 @@ namespace ShareDeployed.Services
 	public interface ICryptoService
 	{
 		string CreateSalt();
+
+		//was extended
+		byte[] Protect(byte[] plainText);
+
+		byte[] Unprotect(byte[] payload);
 	}
 
 	public class CryptoService : ICryptoService
 	{
+		private IKeyProvider _provider;
+
+		public CryptoService(IKeyProvider provider)
+		{
+			if (provider == null)
+				throw new ArgumentNullException("provider");
+			_provider = provider;
+		}
+
 		public string CreateSalt()
 		{
 			var data = new byte[0x10];
@@ -20,6 +35,21 @@ namespace ShareDeployed.Services
 
 				return Convert.ToBase64String(data);
 			}
+		}
+
+		public byte[] Protect(byte[] plainText)
+		{
+			var initializationVector = new byte[16];
+			using (var crypto = new RNGCryptoServiceProvider())
+			{
+				crypto.GetBytes(initializationVector);
+				return CryptoHelper.Protect(_provider.EncryptionKey, _provider.VerificationKey, initializationVector, plainText);
+			}
+		}
+
+		public byte[] Unprotect(byte[] payload)
+		{
+			return CryptoHelper.Unprotect(_provider.EncryptionKey, _provider.VerificationKey, payload);
 		}
 	}
 }
